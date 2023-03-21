605e4343745f6f51dd0a30e834043494e5c30b7b
annalisemshaw <sha19023@byui.edu>
2023-03-20 18:22:32 -0700

    Update font import

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Valentine&apos;s Pizzeria</title>
         <link href="https://fonts.googleapis.com/css2?family=Ultra&display=swap" rel="stylesheet" />
       </Head>
       <body>
--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,7 @@ export default function Home() {
   return (
       <>
         <Head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
           <title>Valentine&apos;s Pizzeria</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         </Head>
         <Navbar cartCount={cartCount} />
         <Hero />
